91f4629fc84243cc2bab081f43ca425d7fdabcee
Bulentor <148325792+Bulentor@users.noreply.github.com>
2023-11-08 10:36:06 +0000

    test completed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
-}
+  const arr = [];
+  for (let i = 0; i < arraySize; i += 1) {
+    arr.push(value);
+  }
+  return arr;
+};
 
 const data = 3;
 const valueToFill = 'a';
@@ -9,16 +13,22 @@ console.log(fill(data, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  return array.reverse()
+};
 
 const data = [1, 2, 3];
 console.log(reverse(data)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
-}
+  const result = [];
+  for (i = 0; i < array.length; i += 1) {
+    if (array[i] !== 0 && Boolean(array[i]) === true) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
 console.log(compact(data)) // [1, 2, 3]
